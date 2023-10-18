041c4c2e19507bdf8d4a883eef78607a2061c23f
YGL <139696084+yeogeru@users.noreply.github.com>
2023-10-18 15:23:35 +0900

    Create BOJ16987.java

---
+++ BOJ/G5/BOJ16987.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+ * 
+ * @author paternalism532
+ *
+ *	브루트포스, 백트래킹
+ */
+public class BOJ16987 {
+	static int n, atk[], maxtotal = 0;
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	n = Integer.parseInt(br.readLine());
+    	atk = new int[n];
+    	int[] hp = new int[n];
+    	for(int i = 0 ; i < n ; i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    		hp[i] = Integer.parseInt(st.nextToken());
+    		atk[i] = Integer.parseInt(st.nextToken());
+    	}
+    	breakEgg(0, hp);
+    	System.out.println(maxtotal);
+    }
+    static void breakEgg(int index, int[] egg) {
+    	if(index == n) {
+    		int total = 0;
+    		for(int i = 0 ; i < n ; i++) {
+    			if(egg[i] <= 0) total++;
+    		}
+    		maxtotal = Math.max(maxtotal, total);
+    		return;
+    	}
+    	if(egg[index] <= 0) {
+    		breakEgg(index+1, egg);
+    	} else {
+    		boolean eggbroken = true;
+    		for(int i = 0 ; i < n ; i++) {
+    			if(i==index) continue;
+    			if(egg[i] > 0) {
+    				int[] newEgg = new int[n];
+    				for(int j = 0 ; j < n ; j++) {
+    					newEgg[j] = egg[j];
+    				}
+    				eggbroken = false;
+    				newEgg[i]-=atk[index];
+    				newEgg[index]-=atk[i];
+    				breakEgg(index+1, newEgg);
+    			}
+    		}
+    		if(eggbroken) breakEgg(index+1, egg);
+    	}
+    }
+}
