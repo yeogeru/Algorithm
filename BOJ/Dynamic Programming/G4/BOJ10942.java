aa907832c4fb5f38a2c7c918c902caf8039e14da
YGL <139696084+yeogeru@users.noreply.github.com>
2023-10-23 23:49:53 +0900

    Create BOJ10942.java

---
+++ BOJ/Dynamic Programming/G4/BOJ10942.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+/**
+ * @author yeogeru
+ *	  Dynamic Programming
+ */
+public class BOJ10942 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		String[] input = (br.readLine()).split(" ");
+		int[] arr = new int[n];
+		for(int i = 0 ; i < n ; i++) {
+			arr[i] = Integer.parseInt(input[i]);
+		}
+		int m = Integer.parseInt(br.readLine());
+		Loop:
+		for(int i = 0 ; i < m ; i++) {
+			input = (br.readLine()).split(" ");
+			int a = Integer.parseInt(input[0]);
+			int b = Integer.parseInt(input[1]);
+			for(int x = a-1, y = b-1, cnt = 0 ; cnt <= (b-a)/2 ; x++, y--, cnt++) {
+				if(arr[x]!=arr[y]) {
+					bw.write("0\n");
+					continue Loop;
+				}
+			}
+			bw.write("1\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
