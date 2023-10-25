febc13237167daabee6f95860912347b8fb45c57
YGL <139696084+yeogeru@users.noreply.github.com>
2023-10-26 08:37:22 +0900

    Create BOJ1094.java

---
+++ BOJ/Bit Masking/S5/BOJ1094.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/**
+ * @author yeogeru
+ *	  Bit Masking
+ */
+public class BOJ1094 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String x = Integer.toBinaryString(Integer.parseInt(br.readLine()));
+		int total = 0;
+		for(int i = 0 ; i < x.length() ; i++) {
+			if(x.charAt(i)=='1') ++total;
+		}
+		System.out.println(total);
+	}
+}
