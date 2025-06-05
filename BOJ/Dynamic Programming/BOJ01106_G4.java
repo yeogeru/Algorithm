1f22d531b64aaa51b4243c2767e94f23208b4922
yeogeru <paternalism532@gmail.com>
2025-06-05 19:35:32 +0900

    solved at 0605

--- BOJ/Dynamic Programming/BOJ01106_G4.java
+++ BOJ/Dynamic Programming/BOJ01106_G4.java
@@ -1,12 +1,14 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 /**
  * @author : Yeogeru
  * @description : Dynamic Programming
  * @since : 2025-06-05
  */
-public class Main {
+public class BOJ01106_G4 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
