dc9c41d8dd3369239eab9b8ec2a6ea45ad20e5f2
yeogeru <paternalism532@gmail.com>
2025-07-06 22:01:00 +0900

    solved at 0706

--- BOJ/Dynamic Programming/BOJ01535_S2.java
+++ BOJ/Dynamic Programming/BOJ01535_S2.java
@@ -1,12 +1,13 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * @author : Yeogeru
  * @description : Dynamic Programming
  * @since : 2025-07-06
  */
-public class Main {
+public class BOJ01535_S2 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
