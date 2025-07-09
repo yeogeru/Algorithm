c5a2905d8b272153edb02d16d60178e29e5edc22
yeogeru <paternalism532@gmail.com>
2025-07-09 09:41:45 +0900

    solved at 0709

--- BOJ/Implementation/BOJ20207_G5.java
+++ BOJ/Implementation/BOJ20207_G5.java
@@ -1,4 +1,5 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /**
@@ -6,7 +7,7 @@ import java.util.*;
  * @description : Implementation, Greedy
  * @since : 2025-07-09
  */
-public class Main {
+public class BOJ20207_G5 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
