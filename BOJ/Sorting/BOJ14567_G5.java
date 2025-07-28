98649dab091bfac3db485f2a13ee5c81e5ec0e88
yeogeru <paternalism532@gmail.com>
2025-07-28 15:39:21 +0900

    solved at 0728

--- BOJ/Sorting/BOJ14567_G5.java
+++ BOJ/Sorting/BOJ14567_G5.java
@@ -1,4 +1,5 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /**
@@ -6,7 +7,7 @@ import java.util.*;
  * @description : Topology Sort
  * @since : 2025-07-28
  */
-public class Main {
+public class BOJ14567_G5 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
