70293162a78efd4ef92957f6ad51cb427b7ea16f
yeogeru <paternalism532@gmail.com>
2025-07-29 13:33:03 +0900

    solved at 0729

--- Main/Main.java
+++ Main/Main.java
@@ -3,59 +3,46 @@ import java.util.*;
 
 /**
  * @author : Yeogeru
- * @description : Topology Sort
- * @since : 2025-07-28
+ * @description : Dynamic Programming
+ * @since : 2025-07-29
  */
 public class Main {
+    static final int MOD = 10007;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] depth = new int[n + 1];
+        int h = Integer.parseInt(st.nextToken());
         List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
-        }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            list.get(a).add(b);
-            ++depth[b];
-        }
-
-        Queue<Integer> que = new ArrayDeque<>();
+        list.add(new ArrayList<>());
         for (int i = 1; i <= n; i++) {
-            if (depth[i] == 0) que.add(i);
+            st = new StringTokenizer(br.readLine(), " ");
+            list.add(new ArrayList<>());
+            list.get(i).add(0);
+            while (st.hasMoreTokens()) {
+                int input = Integer.parseInt(st.nextToken());
+                list.get(i).add(input);
+            }
         }
 
-        int[] result = new int[n + 1];
-        int count = 1;
-        boolean changed = true;
-        while (changed) {
-            changed = false;
-            int qSize = que.size();
-            for(int q = 0 ; q < qSize ; q++) {
-                int t = que.poll();
-                result[t] = count;
-                for (int i : list.get(t)) {
-                    if (--depth[i] == 0) {
-                        que.add(i);
-                        changed = true;
-                    }
+        int[][] dp = new int[n + 1][h + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= h; j++) {
+                for (int k : list.get(i)) {
+                    if (j < k) continue;
+                    dp[i][j] += dp[i - 1][j - k];
+                    dp[i][j] %= MOD;
                 }
             }
-            ++count;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            sb.append(result[i]);
-            if (i != n) sb.append(" ");
-        }
-
-        System.out.println(sb);
+        System.out.println(dp[n][h]);
 
     }
 
