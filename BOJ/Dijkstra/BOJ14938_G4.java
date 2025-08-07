e110610b273c27284874895abbfbc325781835a5
yeogeru <paternalism532@gmail.com>
2025-08-07 21:34:56 +0900

    solved at 0807

--- BOJ/Dijkstra/BOJ14938_G4.java
+++ BOJ/Dijkstra/BOJ14938_G4.java
@@ -1,4 +1,5 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /**
@@ -6,7 +7,7 @@ import java.util.*;
  * @description : Dijkstra
  * @since : 2025-08-07
  */
-public class Main {
+public class BOJ14938_G4 {
     static class Edge implements Comparable<Edge> {
         int to, weight;
 
