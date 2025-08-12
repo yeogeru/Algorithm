f24c18b7b4e49ceddd0e906ea422261e07ff1bd3
yeogeru <paternalism532@gmail.com>
2025-08-12 15:34:51 +0900

    solved at 0812

--- BOJ/Implementation/BOJ14891_G5.java
+++ BOJ/Implementation/BOJ14891_G5.java
@@ -1,12 +1,13 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * @author : Yeogeru
  * @description : Implementation
  * @since : 2025-08-12
  */
-public class Main {
+public class BOJ14891_G5 {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
