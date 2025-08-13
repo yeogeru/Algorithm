b8805c10be4fa6a9f08bf728118ba90a65418b59
yeogeru <paternalism532@gmail.com>
2025-08-13 14:58:33 +0900

    solved at 0813

--- BOJ/Implementation/BOJ15683_G3.java
+++ BOJ/Implementation/BOJ15683_G3.java
@@ -1,12 +1,15 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 /**
  * @author : Yeogeru
  * @description : Implementation
  * @since : 2025-08-13
  */
-public class Main {
+public class BOJ15683_G3 {
     static Map<Integer, int[]> map = new HashMap<>();
     static int n, m;
     static int[][] graph;
