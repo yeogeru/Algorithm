460ec6f45a3959bcf1c28f80de17f67edaa648a0
yeogeru <paternalism532@gmail.com>
2025-12-31 16:23:02 +0900

    solved at 1231

--- BOJ/ETC/BOJ11657_G4.java
+++ BOJ/ETC/BOJ11657_G4.java
@@ -6,7 +6,7 @@ import java.io.*;
  * @description : Bellman-Ford
  * @since : 2025-12-31
  */
-public class Main {
+public class BOJ11657_G4 {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
